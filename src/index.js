408e29c7556718cd21ffa506dcd2c8f8c7d2ff33
mayinmarch <marfa.astafieva@gmail.com>
2020-03-06 03:30:15 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let numbers = [];
+    let reversed = 0;
+    n = Math.abs(n);
+
+    while (n > 0) {
+        numbers.push(n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    for (let i = 0; i < numbers.length; i ++) {
+        reversed +=  numbers[i] * Math.pow(10, (numbers.length - i) - 1);
+    }
+    return reversed;
 }
